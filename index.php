fcfb611b15722dea3c7c4190174ca76f5a77f252
Ramya <ramya3243@gmail.com>
2017-05-01 15:02:20 +0530

    v2-2

---
+++ index.php
@@ -0,0 +1,326 @@
+<html>
+<head>
+        <title>Machine Learning Week - UpX Academy</title>
+        <meta charset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="ex.css" rel="stylesheet">
+        <link href="ex.css" rel="stylesheet">
+        <link rel="stylesheet" href="ex.css">
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1594657153896431'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<noscript>&lt;img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1594657153896431&amp;ev=PageView&amp;noscript=1"
+/&gt;</noscript>
+ <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?3xJLN2tMYFAKr4IbVCGxBdsH7zU3zmhQ";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+
+<script type="text/javascript"> var MXLandingPageId = 'b27a250a-2377-11e7-996c-22000aa41726'; </script>
+<script type="text/javascript" src="https://web.mxradon.com/t/Tracker.js"></script>
+<script type="text/javascript">
+      pidTracker('13870');
+</script>
+    </head>
+    <body>
+    
+
+<script>
+fbq('track', 'Lead', {
+value: 10.00,
+currency: 'INR'
+});
+</script>
+
+
+    
+        <header>
+           <img src="img/11.png" alt="" class="top-link">
+            <div class="container">
+               
+                <div class="header-content">
+                    <img src="img/12.png" alt="" class="top-logo img-responsive">
+                    <div class="heading">
+                        <h1 class="upper-text white-text square"><span class="gothic black-text">CRACKING THE</span> MACHINE LEARNING CODE</h1>
+                    </div>
+                    
+                    <div class="content">
+                        <p class="raleway white-text">From powering self driving cars and recommendation engines, 
+                                                       to enhancing cybersecurity, machine learning is one of the 
+                                                        biggest breakthroughs of the digital age. Sign up for a week long 
+                                                        lecture series and deep dive into the fascinating world of the 
+                                                        hottest technology today.
+                        </p>
+                    </div>
+                    
+                    <div style="margin-top:25px">
+                    
+                        <img src="img/13.png" alt="" class="img-responsive">
+                    </div>
+                </div>
+                
+                <div class="form-div-holder" style="padding-bottom:0px">
+                    <div class="form-holder">
+                        <h4 class="white-text text-center roboto light-text">For Enquiries</h4>
+                        <div class="second-line">
+                           
+                            <img src="img/14.png" alt="" class="call-logo img-responsive">
+                            <h3 class="white-text text-center roboto light-text">1800-123-1260</h3>
+                        </div>
+                        <form action="Freeregister" class="the-form" data-mxform-id="1" style="margin-bottom:0px" name="FORM 0" method="post" enctype="multipart/form-data">
+                            <h4 class="upper-text raleway text-center">Online, Live &amp; Interactive</h4>
+                            <h5 class="orange-text text-center gothic black-text">30th APRIL 2017 - 6th MAY 2017</h5>
+                            <input type="text" placeholder="First Name" class="form-input gothic" data-old-name="first_name" data-name="FirstName" data-is-mapped="1" required="required" name="FirstName">
+                            <input type="text" placeholder="Last Name" class="form-input gothic" data-old-name="last_name" data-name="LastName" data-is-mapped="1" required="required" name="LastName">
+                            <input type="email" placeholder="Email" class="form-input gothic" data-old-name="email" data-name="EmailAddress" data-is-mapped="1" required="required" name="EmailAddress">
+                            <input type="tel" pattern="\d\d\d\d\d\d\d\d\d\d+" placeholder="Phone" class="form-input gothic" data-old-name="phone" data-name="Phone" data-is-mapped="1" required="required" name="Phone">
+                            <input type="hidden" name="MXHOrgCode" value="13870">
+                            <input type="hidden" name="MXHLandingPageId" value="b27a250a-2377-11e7-996c-22000aa41726">
+                            <input type="hidden" name="MXHFormBehaviour" value="1">
+                            <input type="hidden" name="MXHOutputMessagePosition" value="0">
+                            <input type="hidden" name="MXHFormDataTransfer" value="0">
+                            <input type="hidden" name="MXHRedirectUrl" value="http://events.upxacademy.com/Thank-You-Page">
+                            <input type="hidden" name="MXHAsc" value="5">
+                            <input type="hidden" name="MXHFormName" value="FORM 0">
+                            <input type="submit" class="form-input orange-button raleway bold-text" value="Register Free">
+                        </form>
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+        
+        <section id="why-attend">
+            <div class="container">
+               <div class="section-heading">
+                   <img src="img/11.png" alt="" class="img-responsive">
+                   <h1 class="white-text text-center upper-text roboto light-text">Why Attend?</h1>
+               </div>
+                <div class="reasons">
+                    <div class="reason">
+                        <img src="img/1.png" alt="" class="img-responsive center-block">
+                        <p class="raleway text-center">Know about the opportunities &amp;
+growth prospects in a 
+Machine Learning career</p>
+                    </div>
+                    <div class="reason">
+                        <img src="img/15.png" alt="" class="img-responsive center-block">
+                        <p class="raleway text-center bold-text">Understand the emerging trends
+&amp; learning paths in 
+Machine Learning ecosystem</p>
+                    </div>
+                    <div class="reason">
+                        <img src="img/16.png" alt="" class="img-responsive center-block">
+                        <p class="raleway text-center">Get insights about applications &amp;
+case studies from best minds in
+the industry</p>
+                    </div>
+                    <div class="reason">
+                        <img src="img/17.png" alt="" class="img-responsive center-block">
+                        <p class="raleway text-center">Get career advise &amp; tips to
+crack Machine Learning roles</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section id="speakers-list">
+            <div class="container">
+                <div class="section-heading">
+                    <img src="img/18.png" alt="" class="img-responsive">
+                    <h1 class="orange-text upper-text text-center light-text roboto">Speakers</h1>
+                </div>
+                <div class="speakers">
+                    <div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Mukesh Jain</h4>
+                        <p class="white-text text-center raleway light-text">Ex-VP &amp; Head <br>Big Data Analytics<br>Reliance Jio</p>
+                    </div>
+                    <div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Ratnakar Pandey</h4>
+                        <p class="white-text text-center raleway light-text">India Head,Analytics<br>Kabbage Inc <br>MBA, ISB</p>
+                    </div>
+                    <div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Rajeev Kumar</h4>
+                        <p class="white-text text-center raleway light-text">Head Google Projects<br>
+IBM</p>
+                    </div>
+                    <div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Rajath K M</h4>
+                        <p class="white-text text-center raleway light-text">Machine Learning Specialist<br>
+IIT Madras<br>
+Hyperverge Inc</p>
+                    </div>
+                    <div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Akshay Gupta</h4>
+                        <p class="white-text text-center raleway light-text">Senior Research Engineer<br>
+Applied Machine Learning<br>E-bay</p>
+                    </div>
+                    <div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Kunal Jain</h4>
+                        <p class="white-text text-center raleway light-text">Founder
+<br>Analytics Vidhya<br>IIT, Bombay </p>
+                    </div>
+                    <div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Manvender Singh
+</h4>
+                        <p class="white-text text-center raleway light-text">Founder &amp; CEO<br>UpX Academy<br>MBA, ISB</p>
+                    </div>
+
+<div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Suchit Majumdar</h4>
+                        <p class="white-text text-center raleway light-text">IoT &amp; Smart City consultant <br>
+with AP Government<br>MBA, ISB</p>
+                    </div>
+<div class="speaker">
+                        <h4 class="raleway text-center white-text bold-text">Arun K</h4>
+                        <p class="white-text text-center raleway light-text">Healthcare Analytics Expert<br>University of Cologne</p>
+                    </div>
+
+
+                </div>
+            </div>
+        </section>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        <section id="agenda">
+            <div class="container">
+                <div class="section-heading" style="">
+                    <h1 class="text-center roboto light-text">Agenda</h1>
+                </div>
+                <div class="agenda-items">
+                    <div class="agenda-item">
+                        <img src="img/20.svg" alt="" class="img-responsive center-block">
+                        <h4 class="text-center roboto grey-text light-text">Introduction to Machine Learning</h4>
+                    </div><div class="agenda-item">
+                        <img src="img/21.svg" alt="" class="img-responsive center-block">
+                        <h4 class="text-center roboto grey-text light-text">Applications of ML in Telecom</h4>
+                    </div><div class="agenda-item">
+                        <img src="img/22.svg" alt="" class="img-responsive center-block">
+                        <h4 class="text-center roboto grey-text light-text">Machine Learning at scale in E-commerce</h4>
+                    </div><div class="agenda-item">
+                        <img src="img/23.svg" style="width:80px" alt="" class="img-responsive center-block">
+                        <h4 class="text-center roboto grey-text light-text">Analytics vs <br>Machine Learning vs <br>Artificial  Intelligence</h4>
+                    </div><div class="agenda-item">
+                        <img src="img/24.svg" style="width:80px" alt="" class="img-responsive center-block">
+                        <h4 class="text-center roboto grey-text light-text">Computer Vision for Image Processing</h4>
+                    </div><div class="agenda-item">
+                        <img src="img/25.svg" alt="" class="img-responsive center-block">
+                        <h4 class="text-center roboto grey-text light-text">Stock Prediction using Machine Learning</h4>
+                    </div><div class="agenda-item">
+                        <img src="img/26.svg" alt="" class="img-responsive center-block">
+                        <h4 class="text-center roboto grey-text light-text">Future of Careers in Machine Learning</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        
+        
+        <section id="student-speak">
+            <div class="container">
+                <div class="section-heading">
+                    <h2 class="white-text text-center roboto light-text">UpX Student Speak</h2>
+                </div>
+                <div class="speaks">
+                   <img src="img/27.png" alt="" class="quote img-responsive left-quote">
+                   <img src="img/28.png" alt="" class="quote img-responsive right-quote">
+                    <div class="speak">
+                        <p class="white-text text-justify roboto light-text">UpX Academy has understood the Big Data Analytics space fairly well and are doinga brilliant job in educating 
+young leaders and professionals who look forward to join this sector.</p>
+                        <p class="author white-text text-right roboto light-text">- Jai Kumar - Director, BI, NTT DATA</p>
+                    </div>
+                    <div class="speak">
+                        <p class="white-text text-justify roboto light-text">UpX Academy has excellent faculty on-board that brings in industry perspective in their programs. The five week 
+training they conducted has helped my team adopt the latest Big Data Analytics practices. </p>
+                        <p class="author white-text text-right roboto light-text">- Puneet Sakhuja - BI, Analytics - Practice leader, Tech Mahindra</p>
+                    </div>
+                    <div class="speak">
+                        <p class="white-text text-justify roboto light-text">Initially, we were a bit apprehensive about online trainings. But the interactive approach, support and the way the 
+trainings were organized impressed my team. The pace of the training was perfect and suitable for even beginners 
+in this area.</p>
+                        <p class="author white-text text-right roboto light-text">- Siddharth Bhattacharya - AVP Analytics, Barclays</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="about">
+            <div class="container">
+                <div class="section-heading">
+                    <h1 class="text-center roboto light-text">About UpX Academy</h1>
+                </div>
+                <p class="text-justify roboto light-text">In today’s digital age, data has the potential to transform businesses through insights and analytics. However, this 
+power still awaits to be fully harnessed. As a world-class educational venture, we recognize this challenge and are 
+therefore committed to empower professionals through transformational courses in Big Data and Data Science. 
+</p>
+            </div>
+        </section>
+        
+        <section id="our-courses">
+            <div class="container">
+                <div class="section-heading">
+                    <h2 class="upper-text text-center roboto light-text">Our Courses</h2>
+                </div>
+                <div class="courses">
+                    <div class="course">
+                        <h4 class="text-center raleway black-text white-text upper-text">Big Data</h4>
+                        <h4 class="text-center raleway light-text white-text upper-text">Hadoop</h4>
+                        <h4 class="text-center raleway light-text white-text upper-text">Apache Spark</h4>
+                        <h4 class="text-center raleway light-text white-text upper-text">Mongo DB</h4>
+                        <a href="https://upxacademy.com/" class="orange-button upper-text roboto light-text">Know More</a>
+                        <div class="bg left"></div>
+                        <div class="bg right"></div>
+                    </div>
+                    <div class="course">
+                        <h4 class="text-center raleway black-text white-text upper-text">Data Science</h4>
+                        <h4 class="text-center raleway light-text white-text upper-text">Data Analytics</h4>
+                        <h4 class="text-center raleway light-text white-text upper-text">Machine Learning</h4>
+                        <h4 class="text-center raleway light-text white-text upper-text">Tableau</h4>
+                        <a href="https://upxacademy.com/" class="orange-button upper-text roboto light-text">Know More</a>
+                        <div class="bg left"></div>
+                        <div class="bg right"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <footer>
+            <div class="container">
+                <div class="first">
+                    <a href="http://www.upxacademy.com" class="white-text roboto">www.upxacademy.com</a>
+                    <a href="mailto:info@upxacademy.com" class="white-text roboto">Email: info@upxacademy.com</a>
+                </div>
+                <div class="second">
+                    <p class="white-text roboto text-right">© 2017 Upx. All rights Reserved</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
